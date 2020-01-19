8fd48080bd81f56e8f06ae824e553960421c2351
SophiaMVaughn <cq7614@wayne.edu>
2020-01-19 02:13:29 -0500

    practicing url scraping on a news site; got the urls filtered, working on the names

---
+++ url-scraping-test.py
@@ -0,0 +1,54 @@
+import requests #library that communicates requests from scraper to the web page
+from bs4 import BeautifulSoup
+
+page = requests.get('https://www.usnpl.com/search/state?state=MI')
+bs = BeautifulSoup(page.text, 'html.parser') #instantiate object with these parameters
+#other parsers exist other than this one
+
+allLinksHolder = bs.find(class_='table table-sm') #just the table
+justLinks = allLinksHolder.find_all('a') #all anchor tags within that table
+
+allItalicLinkChildren = allLinksHolder.find_all('i')
+
+##firstClassMatch = bs.find(class_='w-50')
+##allNewsNamesHolder = allLinksHolder.find_all('td')
+##for nameC in allNewsNamesHolder:
+##    if nameC.contents[0] is not None:
+##    #if they have the same class as firstClassMatch
+##        print(nameC.contents[0])
+
+##justNames = allNewsNamesHolder.find_all('a')
+
+##for name in justNames:
+##    n = name.contents[0]
+##    print(n)
+
+
+#THIS LOOP SUCCESSFULLY ONLY MINES THE LINKS TO THE NEWS WEBSITES
+for italicChild in allItalicLinkChildren:
+    z=italicChild.get('class')
+    for link in justLinks:
+        x=link.contents[0]
+        if x is italicChild and italicChild.get('class')[1] == 'fa-link':
+            y = link.get('href')
+            print(y)
+    
+    #ALL OF THESE WERE AN ATTEMPT AT SAYING:
+    #if the child item of the 'a' tag (link.contents[0])
+        #has the class 'fas fa-link', show it, else don't
+    #the problem is that link.contents[0] is recognized as a bs4 Tag object
+        #BUT only until you try to access its attributes, then it becomes a navigblestring object
+        #unsure how that happens but it does so had to give up on that idea for now
+    
+    
+    #if(link.contents[0]!=NoneType)
+        #testsoup = BeautifulSoup(link.contents[0], 'html.parser')
+    
+    #x=link.contents[0]
+    #z=x['class'] #<--- this is how the documentations says to access the class of it
+    #print(z)
+
+    #if(x['class']=='fas fa-link')
+        #print the link href, else don't
+    
+    
