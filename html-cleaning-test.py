dced8e196e782e9945d9a967b88117ef2795a5e2
SophiaMVaughn <cq7614@wayne.edu>
2020-01-19 19:04:13 -0500

    Explaining the issue for explaniding this scripot to multiple sites

--- html-cleaning-test.py
+++ html-cleaning-test.py
@@ -3,7 +3,7 @@ import requests
 
 page = requests.get('https://www.lenconnect.com/news/20200118/overnight-winter-storm-turns-slushy')
 bs = BeautifulSoup(page.text, 'html.parser')
-content=bs.find(class_='article-body')
+content=bs.find(class_='article-body')  #THIS IS THE MAIN PROBLEM (the developer has to inspect the html and find the container class for that specific page for this to work
 splitContent = content.find_all('p')
 for block in splitContent:
     if block.contents and block.contents[0] != '':
