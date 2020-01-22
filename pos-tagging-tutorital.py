9d75a7b9ed91ebe84725de355375d7fa87f5ecf5
SophiaMVaughn <cq7614@wayne.edu>
2020-01-22 11:28:43 -0500

    Figured out why parts of speech were being tagged wrong (I was using incorrect acronyms)

--- pos-tagging-tutorital.py
+++ pos-tagging-tutorital.py
@@ -5,9 +5,11 @@ nlp = spacy.load("en_core_web_sm")
 matcher = Matcher(nlp.vocab)
 # Add match ID "HelloWorld" with no callback and one pattern
 pattern = [{"LOWER": "hello"}, {"IS_PUNCT": True}, {"LOWER": "world"}]
+pattern1 = [{"POS":"NOUN"}, {"POS":"ADP"}]
 matcher.add("HelloWorld", None, pattern)
+matcher.add("p1",None,pattern1)
 
-doc = nlp("Hello, world! Hello world!")
+doc = nlp("Hello, world! Hello world! The chemical was spilled on I-696 on purpose by an evil wizard officials stated.")
 matches = matcher(doc)
 for match_id, start, end in matches:
     string_id = nlp.vocab.strings[match_id]  # Get string representation
