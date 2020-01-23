ff0e3041b7b8b6758dcd207ae8b3688302b2894e
SophiaMVaughn <cq7614@wayne.edu>
2020-01-23 09:54:56 -0500

    Added training script. Set up for preprocessing training data into enitity sections.

---
+++ NER-training.py
@@ -0,0 +1,24 @@
+import spacy
+
+#TODO:
+    ##CREATE OUR OWN NAMED ENTITIES - subloc, watbod, chem, titles, units
+
+TRAIN_DATA = [
+    #EXAMPLE FORMAT
+    #("I like London and Berlin.", {"entities": [(7, 13, "LOC"), (18, 24, "LOC")]}),
+##CITIES------------------------------------- (LOC)
+
+##DATES-------------------------------------- (DATE)
+
+##LOCATIONS (streets) --------------------------------------
+
+##BODIES OF WATER ---------------------------------------------
+
+##CHEMICALS-----------------------------------------
+
+##ORGANIZTIONS------------------------------------- (ORG)
+
+##OFFICIAL TITLES?-------------------------------------
+
+##UNITS--------------------------------------------------
+]
