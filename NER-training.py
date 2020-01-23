222c54231c92d40575955bc076141bbd807ce28a
SophiaMVaughn <cq7614@wayne.edu>
2020-01-23 13:14:24 -0500

    More setup for this functionality (comments)

--- NER-training.py
+++ NER-training.py
@@ -9,6 +9,9 @@ ENT3 = "CHEM"
 ENT4 = "TITLE"
 ENT5 = "UNIT"
 
+#MIX IN EXAMPLES OF RECOGNIZED TYPES (ON TOP OF THE NEW ONES)
+    #don't worry about the order, the training process shuffles I think
+#MIX IN SENTENCES THAT CONTAIN NONE OF THESE ENTITIES AND TELL THE MODEL THAT
 TRAIN_DATA = [
     #EXAMPLE FORMAT
     #("I like London and Berlin.", {"entities": [(7, 13, "LOC"), (18, 24, "LOC")]}),
