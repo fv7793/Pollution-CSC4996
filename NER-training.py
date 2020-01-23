ba3bc14679a7e931835b6e6f534a05867a03bf27
SophiaMVaughn <cq7614@wayne.edu>
2020-01-23 10:05:36 -0500

    Added the custom entity names

--- NER-training.py
+++ NER-training.py
@@ -3,6 +3,12 @@ import spacy
 #TODO:
     ##CREATE OUR OWN NAMED ENTITIES - subloc, watbod, chem, titles, units
 
+ENT1 = "SUBLOC"
+ENT2 = "BODWAT"
+ENT3 = "CHEM"
+ENT4 = "TITLE"
+ENT5 = "UNIT"
+
 TRAIN_DATA = [
     #EXAMPLE FORMAT
     #("I like London and Berlin.", {"entities": [(7, 13, "LOC"), (18, 24, "LOC")]}),
