4a6e3667ac9660300f4ce9d945f4ad5cd3bb0333
SophiaMVaughn <cq7614@wayne.edu>
2020-02-05 16:59:59 -0500

    Update regExFuncs.py

--- regExFuncs.py
+++ regExFuncs.py
@@ -1,9 +1,11 @@
+
 #function to take a sentence and return the first date pattern in it
 
 
 #function to take a sentence and see if it contains address/st
 #optional num + proper noun/noun + st/rd/ave/etc
 #I/M - #
+
 
 
 
