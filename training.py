4b7c6b0b4ebf9077671248083bdc6a21d422ed6e
SophiaMVaughn <cq7614@wayne.edu>
2020-02-05 23:48:53 -0500

    Made a parser for the CSV info (need to add POS to each tuple and also to the writing CSV)

---
+++ training.py
@@ -0,0 +1,112 @@
+import sklearn
+
+import sklearn_crfsuite
+from sklearn_crfsuite import scorers
+from sklearn_crfsuite import metrics
+
+t_s = []
+f = open("CSVtest.csv","r")
+line = f.readline()
+cnt = 1
+
+sent = []
+while line:
+    firstWord = False
+   #print("Line {}: {}".format(cnt, line.strip()))
+    if(line[0]=="S"): #start of a new sentence
+        if len(sent)!=0:
+            t_s.append(sent)
+            print(sent)
+            sent = []
+
+    #test for ,,, FIRST (if it doesn't find it, use the general case)
+    if ",,," in line:
+        tuple = (",",line[-3])
+        sent.append(tuple)
+    #else we know this won't be a comma so we can just use split
+    else:
+        split = line.split(",")
+        tuple = (split[1],split[2])
+        sent.append(tuple)
+    #put it in a tuple and append to sent
+        
+    line = f.readline()
+    cnt += 1
+
+
+
+#if the line has sentence # in the column, start a new sentence list
+#make a tuple for each line, put it in the current sentence object
+
+#t_s is a list of sentences
+#each sentence should be
+#[
+#(word,tag),
+#(word,tag), ...
+#]
+
+
+#parse the CSV into t_s
+
+def word2features(sent, i):
+    word = sent[i][0]
+
+    features = {
+        'bias': 1.0,
+        'word.lower()': word.lower(),
+        'word[-3:]': word[-3:],
+        'word[-2:]': word[-2:],
+        'word.isupper()': word.isupper(),
+        'word.istitle()': word.istitle(),
+        'word.isdigit()': word.isdigit(),
+    }
+    if i > 0:
+        word1 = sent[i-1][0]
+        features.update({
+            '-1:word.lower()': word1.lower(),
+            '-1:word.istitle()': word1.istitle(),
+            '-1:word.isupper()': word1.isupper()
+        })
+    else:
+        features['BOS'] = True
+
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        features.update({
+            '+1:word.lower()': word1.lower(),
+            '+1:word.istitle()': word1.istitle(),
+            '+1:word.isupper()': word1.isupper()
+        })
+    else:
+        features['EOS'] = True
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+def sent2labels(sent):
+    return [label for token, label in sent]
+
+def sent2tokens(sent):
+    return [token for token, label in sent]
+
+
+
+X_train = [sent2features(s) for s in t_s]
+y_train = [sent2labels(s) for s in t_s]
+
+X_test = [sent2features(s) for s in t_s]
+y_test = [sent2labels(s) for s in t_s]
+
+crf = sklearn_crfsuite.CRF(
+    algorithm='lbfgs',
+    c1=0.1,
+    c2=0.1,
+    max_iterations=100,
+    all_possible_transitions=True
+)
+crf.fit(X_train, y_train)
+
+
