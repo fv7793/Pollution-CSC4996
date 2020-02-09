c0e24b2de5e9ccc127ca667c12d5ee60114d7f18
SophiaMVaughn <cq7614@wayne.edu>
2020-02-09 04:08:37 -0500

    Takes care of reading \s from the file

--- RNN.py
+++ RNN.py
@@ -1,53 +1,3 @@
-import spacy
-import en_core_web_sm
-
-nlp = en_core_web_sm.load()
-
-def convertScrapedtoSent(splitContent):
-    tokenizedSent = []
-    #tokenize
-    NLPtxt = nlp(splitContent)
-    for eachSent in NLPtxt.sents:
-        tokenizedSent.append(eachSent.string.strip())
-    return tokenizedSent
-
-def paragraphToWords(splitContent):
-    tokenizedWords = []
-    POS = []
-    NLPtxt = nlp(splitContent)
-    for eachWord in NLPtxt:
-        tokenizedWords.append(eachWord.text)
-        POS.append(eachWord.tag_)
-    return tokenizedWords, POS
-
-
-def newsTextToCSV(text, CSVfile):
-    #open CSV
-
-#TODO: REMOVE COMMAS FROM THE GIVEN TEXT BEFORE INSERTING INTO CSV
-    
-    file = open(CSVfile, 'a+')
-    tS = convertScrapedtoSent(text)
-    sentNum = 1
-    for sent in tS:
-        tW, POS = paragraphToWords(sent)
-        firstWord = True
-        for i in range(len(tW)):
-            CSVline = ""
-            if(firstWord==True):
-                CSVline = "Sentence "+str(sentNum)+","
-                firstWord=False
-            else:
-                CSVline = "NaN,"
-            if tW[i]==','or tW[i]=='\\':
-                continue
-            CSVline = CSVline+tW[i]+","+POS[i]+",O,\n"
-            #write to file
-            file.write(CSVline)
-        
-        sentNum = sentNum+1
-    file.close()
-
 def openAndParseCSV(CSVfile):
     csv = open(CSVfile,"r")
     line = csv.readline()
@@ -71,10 +21,12 @@ def openAndParseCSV(CSVfile):
             split = line.split(",")
             if '\\' in split[len(split)-4]:
                 removeBS = split[len(split)-4].split('\\')
-                tuple = (removeBS[1][0],removeBS[1][0],split[len(split)-2])
-                sent.append(tuple)
-                tuple = (removeBS[1][1:],split[len(split)-3],split[len(split)-2])
-                sent.append(tuple)
+                #print(removeBS)
+                if(removeBS[1]!=''):
+                    tuple = (removeBS[1][0],removeBS[1][0],split[len(split)-2])
+                    sent.append(tuple)
+                    tuple = (removeBS[1][1:],split[len(split)-3],split[len(split)-2])
+                    sent.append(tuple)
             else:
                 tuple = (split[len(split)-4],split[len(split)-3],split[len(split)-2])
                 sent.append(tuple)
@@ -89,14 +41,10 @@ def openAndParseCSV(CSVfile):
 CSVcontent = openAndParseCSV("CSVtags.csv")
 # format of CSV content:
 #[ [(word,POS,tag),...],...,[(word,POS,tag),...] ]
-wholeFile = ""
 for sentence in CSVcontent:
     for wordLine in sentence:
-        #print(wordLine)
-        wholeFile=wholeFile+" "+wordLine[0]
+        print(wordLine)
 
-#print(wholeFile)
-newsTextToCSV(wholeFile, "CSVtext.csv")
 
 
 
