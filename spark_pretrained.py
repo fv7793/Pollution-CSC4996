bbc9d1225646e1caca3fb8f46ffb62fe6dfef908
SophiaMVaughn <cq7614@wayne.edu>
2020-02-11 13:48:18 -0500

    Trying to get the java errors to stop

--- spark_pretrained.py
+++ spark_pretrained.py
@@ -1,14 +1,15 @@
 import sparknlp
 from sparknlp.pretrained import PretrainedPipeline
-
+import os
+import sys
+# Path for spark source folder
+os.environ['JAVA_HOME'] = 'C:\Program Files\Java\jre1.8.0_241'
 #create or get Spark Session
-
+print("before start")
 spark = sparknlp.start()
-
-sparknlp.version()
-spark.version
-
+print("after start")
 #download, load, and annotate a text by pre-trained pipeline
 
-pipeline = PretrainedPipeline('recognize_entities_dl', 'en')
+pipeline = PretrainedPipeline('analyze_sentiment_ml', 'en')
+print("after pipeline creation")
 result = pipeline.annotate('Harry Potter is a great movie')
