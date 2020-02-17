48c0b6873c97618eff3dceb5358a16a7999a9286
SophiaMVaughn <cq7614@wayne.edu>
2020-02-17 02:05:19 -0500

    Started the file that will be used to find the data from the article body

---
+++ dataLocation.py
@@ -0,0 +1,7 @@
+#given the body of the article
+#use POS tagging to find official statement
+#find any date mentioned
+
+#call the RNN on the body (what format should it be in??)
+
+#enter everything into the database
