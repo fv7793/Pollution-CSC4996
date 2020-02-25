2ee9e85b79ce67e30d6fd78ce65aca7a5d07fcd7
Isha <ga4033@wayne.edu>
2020-02-25 11:50:37 -0500

    Date recognition - attempt 2
    From last week - version that only recognizes some date formats

--- regExFuncs.py
+++ regExFuncs.py
@@ -1,14 +1,14 @@
 import re
 
-#function to take a sentence and return the date in it - one format
+#function to take a sentence and return the date in it - multiple formats attempt 1
 
-str = """A chemical was spilled on 10/10/2015"""
+dates = "04-04-2004; 04/04/04; 4/04/04; 4/4/05; April 04, 2004; March 24, 2004; April. 24, 2004; April 24 2004; 24 Mar 2004; 24 March 2004; 4 June. 2004; 24 August, 2010; Mar 22nd, 2006; Jul 21st, 2007; Mar 24th, 2004; Jan 2009; Dec 2009; Oct 2014; 6/2004; 12/2004; 2018; 2019"
 
-#MM/DD/YYYY
-all = re.findall(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", str)
+#multiple formats
+regEx = r'(?:\d{1,2}[-/th|st|nd|rd\s]*)?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z\s,.]*(?:\d{1,2}[-/th|st|nd|rd)\s,]*)?(?:\d{2,4})'
 
-for s in all:
-    print(s)
+result = re.findall(regEx, dates)
+print(result)
 
 
 
